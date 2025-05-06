a6287cf69f4c1366ea08f567f9051e6a43d166c0
Jain_Sanya <116953349+Jfury0127@users.noreply.github.com>
2025-05-06 20:52:05 +0530

    grace bot
    dwsdfsf

--- public/js/t_mark_att.js
+++ public/js/t_mark_att.js
@@ -1,8 +1,11 @@
+const attendancedatediv = document.getElementById("att");
 const attendanceDate = document.querySelector("#att-date");
 const takeAttendanceBtn = document.querySelector(".take-att-btn");
+const modifyAttendanceBtn = document.querySelector(".edit-att-btn");
 const lectureSelect = document.getElementById("select-lect");
 const errorMessage = document.querySelector(".error-msg");
 const ctobeloaded = document.querySelector(".content-to-be-loaded");
+const modify_main_div = document.querySelector(".modify_main_div");
 const tablebody = document.querySelector("tbody");
 const rowsperpage = document.querySelector("#rows-per-page");
 const prevpage = document.querySelector("#prev-page");
@@ -30,82 +33,88 @@ attendanceDate.textContent = `${todayFormatted}`
 
 let sectionID;
 let subID;
-let rowsData=[];
+let rowsData = [];
 let attendanceStatus;
 
 takeAttendanceBtn.addEventListener("click", () => {
+
     if (lectureSelect.value == "") {
         errorMessage.classList.remove("hidden"); // Show error message
     } else {
+        modify_main_div.classList.add('hidden');
         errorMessage.classList.add("hidden"); // Hide error message if valid
         // Continue with attendance-taking actions here
         ctobeloaded.classList.remove("hidden");
+        attendancedatediv.classList.remove('hidden');
 
         const lectureSelected = lectureSelect.value;
 
-        sectionID = parseInt(lectureSelected.slice(0,3));
+        sectionID = parseInt(lectureSelected.slice(0, 3));
         subID = lectureSelected.slice(4);
-        
 
-    fetch(`/api/get_students?section_id=${sectionID}&sub_id=${subID}&attendance_date=${todayFormatted}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then(recieved_response => {
-        studentData = recieved_response.stu;
-        stat_array = recieved_response.stat;
-        // console.log(stat_array);
-        rowsData = studentData;
 
-        if(stat_array == 0){
-            let new_status = new Map();
-            // console.log(studentData[i].ENR_NUMBER);
-            for(let i = 0 ;i < studentData.length; i++){
-                new_status.set(studentData[i].ENR_NUMBER,0)
+        fetch(`/api/get_students?section_id=${sectionID}&sub_id=${subID}&attendance_date=${todayFormatted}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
             }
-            attendanceStatus = new_status;
-            // console.log(attendanceStatus);
-        }else{
-            let new_status = new Map();
-            // console.log(stat_array.enr_number);
-            for(let j = 0 ;j < stat_array.length; j++){
-                new_status.set(stat_array[j].enr_number,stat_array[j].status)
-            }
-            attendanceStatus = new_status;
-        }
-        // console.log("rows data" ,rowsData);
-        // Initial render
-        renderPage(1).then(() => setupCheckboxListeners());
-    })
-    .catch(error => {
-        console.error("Error fetching student data:", error);});
-    // Array to store attendance status
-    // Initialize each student with a default status, e.g., "Absent" (false)
-    }   
+        })
+            .then(response => response.json())
+            .then(recieved_response => {
+                studentData = recieved_response.stu;
+                stat_array = recieved_response.stat;
+                // console.log(stat_array);
+                rowsData = studentData;
+
+                if (stat_array == 0) {
+                    let new_status = new Map();
+                    // console.log(studentData[i].ENR_NUMBER);
+                    for (let i = 0; i < studentData.length; i++) {
+                        new_status.set(studentData[i].ENR_NUMBER, 0)
+                    }
+                    attendanceStatus = new_status;
+                    // console.log(attendanceStatus);
+                } else {
+                    let new_status = new Map();
+                    // console.log(stat_array.enr_number);
+                    for (let j = 0; j < stat_array.length; j++) {
+                        new_status.set(stat_array[j].enr_number, stat_array[j].status)
+                    }
+                    attendanceStatus = new_status;
+                }
+                // console.log("rows data" ,rowsData);
+                // Initial render
+                renderPage(1).then(() => setupCheckboxListeners());
+            })
+            .catch(error => {
+                console.error("Error fetching student data:", error);
+            });
+        // Array to store attendance status
+        // Initialize each student with a default status, e.g., "Absent" (false)
+    }
 });
 
-lectureSelect.addEventListener('focus',()=>{
+lectureSelect.addEventListener('focus', () => {
     errorMessage.classList.add("hidden");
 })
 
 //1 Fetch request to get the lecture data
 fetch('/api/teacher_lectures', {
-    method: 'GET', 
+    method: 'GET',
     headers: {
         'Content-Type': 'application/json'
     }
 })
-    .then(response => response.json()) 
+    .then(response => response.json())
     .then(lectureData => {
         // Loop through each item in lectureData and create an option element
         lectureData.forEach(item => {
             const option = document.createElement('option');
             option.value = `${item.SECTION_ID} ${item.SUB_ID}`; // Using SECTION_ID as the option value
-            option.textContent = `${item.SECTION_NAME} - ${item.SUB_NAME}`; 
-            lectureSelect.appendChild(option); 
+            option.textContent = `${item.SECTION_NAME} - ${item.SUB_NAME}`;
+            lectureSelect.appendChild(option);
+
+            modifyAttendanceBtn.click(); // testing
         });
     })
     .catch(error => {
@@ -113,25 +122,21 @@ fetch('/api/teacher_lectures', {
     });
 
 
-// pagination
-
-
-
 let currentpage = 1;
 
 // Handling the next and previous page buttons
-nextpage.addEventListener('click',()=>{
+nextpage.addEventListener('click', () => {
     const rows = rowsperpage.value === "all" ? rowsData.length : parseInt(rowsperpage.value, 10)
-    const totalPages = Math.ceil(rowsData.length /rows);
-    if(currentpage<totalPages) {currentpage++;}
-    else{
-        currentpage=totalPages;
+    const totalPages = Math.ceil(rowsData.length / rows);
+    if (currentpage < totalPages) { currentpage++; }
+    else {
+        currentpage = totalPages;
     }
     renderPage(currentpage).then(() => setupCheckboxListeners());
 })
-prevpage.addEventListener('click',()=>{
-    if(currentpage>1) {currentpage--;}
-    else{
+prevpage.addEventListener('click', () => {
+    if (currentpage > 1) { currentpage--; }
+    else {
         currentpage = 1;
     }
     renderPage(currentpage).then(() => setupCheckboxListeners());
@@ -140,36 +145,36 @@ prevpage.addEventListener('click',()=>{
 
 
 //render data rows acc to page number
-async function renderPage(page){
+async function renderPage(page) {
     tablebody.innerHTML = "";  // Clear previous rows
 
     let rowsperpageValue = rowsperpage.value;
     const rows = rowsperpageValue === "all" ? rowsData.length : parseInt(rowsperpageValue, 10); //no. of rows to be shown
     const startIndex = (page - 1) * rows;
     //rowsData.length can be less than rowsperpageValue selected
-    const endIndex = Math.min(startIndex + rows, rowsData.length);   
+    const endIndex = Math.min(startIndex + rows, rowsData.length);
 
 
     for (let i = startIndex; i < endIndex; i++) {
         const student = rowsData[i];
-        if(student.STU_LNAME == null){
+        if (student.STU_LNAME == null) {
             student.STU_LNAME = "";
         }
-        
-        if(attendanceStatus == 0){
+
+        if (attendanceStatus == 0) {
             var isPresent = false;
-        }else if(attendanceStatus == 1){
+        } else if (attendanceStatus == 1) {
             var isPresent = true;
         }
-        else{
-            if(attendanceStatus.has(student.ENR_NUMBER) ){
+        else {
+            if (attendanceStatus.has(student.ENR_NUMBER)) {
                 const x = attendanceStatus.get(student.ENR_NUMBER);
-                if(x == 0){
+                if (x == 0) {
                     isPresent = false;
-                }else{
+                } else {
                     isPresent = true;
                 }
-            }else{
+            } else {
                 isPresent = false;
             }
         }
@@ -190,8 +195,8 @@ async function renderPage(page){
         // row.classList.add("attendance-row");
     }
 
-    
-    const totalPages = Math.ceil(rowsData.length/rows);
+
+    const totalPages = Math.ceil(rowsData.length / rows);
     pagemsg.textContent = `Page ${page} of ${totalPages}`;
 }
 
@@ -211,14 +216,10 @@ function handleCheckboxClick(e) {
     const index = e.target.getAttribute("data-index");
     const isChecked = e.target.checked;
 
-    // console.log(index);
-    // Update attendanceStatus array
-    // attendanceStatus[index] = isChecked;
-
     // Update the status display in the last cell
     const tablerow = e.target.closest('tr');
     const enr = parseInt(tablerow.firstElementChild.textContent);
-    attendanceStatus.set(enr,isChecked?1:0);
+    attendanceStatus.set(enr, isChecked ? 1 : 0);
     // console.log(attendanceStatus);
     tablerow.lastElementChild.innerHTML = `
         <div class="${isChecked ? 'present h-6 w-16 m-auto bg-green-200 border-2 border-green-400 rounded-md text-center text-green-500' : 'absent h-6 w-16 m-auto bg-red-200 border-2 border-red-400 rounded-md text-center text-red-500'}">
@@ -227,8 +228,8 @@ function handleCheckboxClick(e) {
     `;
 }
 
-markallpresent.addEventListener('click',()=>{
-    if(markallpresent.textContent == "Mark All Present"){
+markallpresent.addEventListener('click', () => {
+    if (markallpresent.textContent == "Mark All Present") {
         // attendanceStatus = 1;
         for (let key of attendanceStatus.keys()) {
             attendanceStatus.set(key, 1);
@@ -236,7 +237,7 @@ markallpresent.addEventListener('click',()=>{
 
         markallpresent.textContent = "Mark All Absent";
     }
-    else if(markallpresent.textContent == "Mark All Absent") {
+    else if (markallpresent.textContent == "Mark All Absent") {
         // attendanceStatus = 0;
         for (let key of attendanceStatus.keys()) {
             attendanceStatus.set(key, 0);
@@ -247,7 +248,7 @@ markallpresent.addEventListener('click',()=>{
     renderPage(currentpage).then(() => setupCheckboxListeners());
 })
 
-saveAtt.addEventListener('click',()=>{
+saveAtt.addEventListener('click', () => {
 
     const attendanceDate = todayFormatted;
     const subjectId = subID;
@@ -265,14 +266,14 @@ saveAtt.addEventListener('click',()=>{
     // Send the data to the backend
     saveAttendance(attendanceData);
 
-    });
+});
 
-    function saveAttendance(data) {
-        fetch('/markAttendance', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-        })
+function saveAttendance(data) {
+    fetch('/markAttendance', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    })
         .then(response => response.json())
         .then(result => {
             if (result.success) {
@@ -283,27 +284,50 @@ saveAtt.addEventListener('click',()=>{
             }
         })
         .catch(err => console.error('Fetch Error:', err));
+}
+
+
+
+// ===========================================================================================================================
+
+// modify attendance
+
+modifyAttendanceBtn.addEventListener("click", () => {
+
+    // if (lectureSelect.value == "") {
+    //     errorMessage.classList.remove("hidden"); 
+    // } else {
+        errorMessage.classList.add("hidden"); 
+        
+        modify_main_div.classList.remove("hidden");
+        ctobeloaded.classList.add('hidden');
+        attendancedatediv.classList.add('hidden');
+        modifyAttendance();
+    // }
+});
+
+
+async function modifyAttendance(){
+    
+    // const lectureSelected = lectureSelect.value;
+    const lectureSelected = 'T1-ECONOMICS FOR ENGINEERS';
+
+    sectionID = parseInt(lectureSelected.slice(0, 3));
+    subID = lectureSelected.slice(4);
+
+    try{
+        //fetch students of that lecture
+
+        const response = await fetch(`/api/get_students?section_id=${sectionID}&sub_id=${subID}&attendance_date=${todayFormatted}`);
+        const studentsoflec = await response.json();
+
+        console.log(studentsoflec);
+
+    }catch(e){
+        console.log("eeor in modification");
+        console.log("error" , e);
     }
 
-
-
-
-// // Function to toggle lecture record
-// function toggleLectureRecording() {
-//     const isChecked = recordCheckbox.checked;
-    
-//     if (isChecked) {
-//         recordCheckbox.checked = false;
-//         attendanceStatus[0] = false;
-//     } else {
-//         recordCheckbox.checked = true;
-//         attendanceStatus[0] = true;
-//     }
-// }
-
-// // Add click listener for both the button and checkbox
-// recordlec.addEventListener("click", toggleLectureRecording);
-// recordCheckbox.addEventListener("click", (e) => {
-//     // e.stopPropagation(); // Prevents the button click event from firing
-//     toggleLectureRecording();
-// });
+    console.log('over');
+    return;
+}
