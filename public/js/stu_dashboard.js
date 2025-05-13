657777a35660bc6fe1fe33244bfb5a3de20487f8
Jain_Sanya <116953349+Jfury0127@users.noreply.github.com>
2025-05-13 20:58:38 +0530

    13-05-25
    students can reupload assignment

--- public/js/stu_dashboard.js
+++ public/js/stu_dashboard.js
@@ -86,14 +86,9 @@ async function load_assign(subId) {
                 : '-';
             
             const submitCell = assignments.Date_of_submission
-                ? `<td class="px-4 py-2 border border-gray-400"><a href="${assignments.Ref_to_submission}" target="_blank" class="text-blue-600 hover:underline">View</a></td>`
-                : `<td class="px-4 py-2 border border-gray-400">
-                    <form id ="my_form_stu_Assign_hidden" onsubmit="submit_assignment_handler(event, '${assignments.Assign_Id}')">
-                        <input type="file" name="ass_sub" required />
-                        <button type="submit" class="h-8 w-24 bg-[#5254dd] hover:bg-[#5254dd]/90 rounded-lg 
-                        cursor-pointer text-white font-medium flex justify-center items-center m-auto"">Submit</button>
-                    </form>
-                </td>`;
+            ? `<td class="px-4 py-2 border border-gray-400"><a href="${assignments.Ref_to_submission}" target="_blank" class="text-blue-600 hover:underline">View</a></td>`
+            : `<td class="px-4 py-2 border border-gray-400">No submission</td>`;
+            
 
             const row = `<tr>
                 <td class="px-4 py-2 border border-gray-400">${i + 1}</td>
@@ -104,6 +99,13 @@ async function load_assign(subId) {
                 <td class="px-4 py-2 border border-gray-400"><a href="${assignments.Ref_to_assignment}" target="_blank" class="text-blue-600 hover:underline">View</a></td>
                 <td class="px-4 py-2 border border-gray-400">${submissionDate}</td>
                 ${submitCell}
+                <td class="px-4 py-2 border border-gray-400">
+                    <form id ="my_form_stu_Assign_hidden" onsubmit="submit_assignment_handler(event, '${assignments.Assign_Id}','${subId}')">
+                        <input type="file" name="ass_sub" required />
+                        <button type="submit" class="h-8 w-24 bg-[#5254dd] hover:bg-[#5254dd]/90 rounded-lg 
+                        cursor-pointer text-white font-medium flex justify-center items-center m-auto"">Submit</button>
+                    </form>
+                </td>
             </tr>`;
             assign_table.insertAdjacentHTML("beforeend", row);
         });
@@ -113,11 +115,22 @@ async function load_assign(subId) {
     }
 }
 
-async function submit_assignment(assignId, file) {
+function getFormattedDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // Month is 0-indexed
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+}
+
+async function submit_assignment(assignId, file,subid) {
     const formData = new FormData();
     formData.append('assignment_id', assignId);
     formData.append('enr_number', enr_num);
     formData.append('assignment_file', file);
+    
+    const date_of_Sub = new Date();
+    const date_send = getFormattedDate(date_of_Sub);
+    formData.append('date', date_send);
 
     try {
         loadingOverlay.style.display = 'flex';
@@ -130,11 +143,14 @@ async function submit_assignment(assignId, file) {
             document.getElementById("spinner").classList.add("hidden");
             document.getElementById("text_loading").textContent = "Assignment submitted successfully!";
             setTimeout(() => {
-                window.location.reload();
+                loadingOverlay.style.display = 'none';
+                load_assign(subid);
+                return;
             }, 2000); // 1000ms = 1 second
-    
+            
         } else {
             alert('Submission failed. Please try again.');
+            loadingOverlay.style.display = 'none';
         }
     } catch (err) {
         console.error("Submission error:", err);
@@ -143,16 +159,12 @@ async function submit_assignment(assignId, file) {
 }
 
 // This handler gets called from the inline form on each row
-function submit_assignment_handler(event, assignId) {
+function submit_assignment_handler(event, assignId,subid) {
     event.preventDefault();
     console.log("from handler",assignId);
     const fileInput = event.target.querySelector('input[name="ass_sub"]');
     const file = fileInput.files[0];
     if (file) {
-        submit_assignment(assignId, file);
+        submit_assignment(assignId, file,subid);
     }
 }
-
-// document.getElementById("my_form_stu_Assign_hidden").addEventListener('submit', () => {
-//     loadingOverlay.style.display = 'flex';
-//   });
